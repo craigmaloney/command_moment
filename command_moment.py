73ef7fcf560da87d3d6e70405145a7ecd62e2033
Craig Maloney <craig@decafbad.net>
2012-03-07 21:55:46 -0500

    Initial randomization foo

--- command_moment.py
+++ command_moment.py
@@ -1,11 +1,19 @@
 import random
-
 import json
 
 class CommandList:
     def __init__(self):
-        self.commands = []
+        self.commands = {}
 
     def load_commands(self):
         file = open('./commands.json', 'r')
         self.commands = json.load(file)
+
+    def active_commands(self):
+        all_true = [key for key, val in self.commands.iteritems() if val]
+        return all_true
+
+    def pick_command(self):
+        active = self.active_commands()
+        random.shuffle(active)
+        return active.pop()
