08b5393cae14a2926ca907b10166fc99dd7d2183
Craig Maloney <craig@decafbad.net>
2012-03-11 12:27:35 -0400

    Added the ability to write the json file

--- command_list.py
+++ command_list.py
@@ -4,10 +4,12 @@ import json
 class CommandList:
     def __init__(self):
         self.commands = {}
+        self.filename = './commands.json'
 
     def load_commands(self):
-        file = open('./commands.json', 'r')
+        file = open(self.filename, 'rt')
         self.commands = json.load(file)
+        file.close()
 
     def active_commands(self):
         all_true = [key for key, val in self.commands.iteritems() if val]
@@ -17,3 +19,11 @@ class CommandList:
         active = self.active_commands()
         random.shuffle(active)
         return active.pop()
+
+    def mark_inactive(self, command):
+        self.commands[command] = False
+
+    def write_commands(self):
+       file = open(self.filename, 'wt')
+       json.dump(self.commands, file, indent=True)
+       file.close()
